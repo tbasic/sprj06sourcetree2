6a2a71ab6f6bc408a864d26791e84f3ed300bf79
HeiKayYoung <kyoungmihaha@gmail.com>
2023-08-04 16:31:20 +0900

    create.song1 & 2 jsp

--- src/main/java/com/tech/sprj06s/song/SongController.java
+++ src/main/java/com/tech/sprj06s/song/SongController.java
@@ -1,8 +1,20 @@
 package com.tech.sprj06s.song;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SongController {
 
+	@RequestMapping("song1")
+	public String song1() {
+	
+		return "song1";
+	}
+	
+	@RequestMapping("song2")
+	public String song2() {
+	
+		return "song2";
+	}
 }
